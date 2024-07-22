5930e1daaa5a8d4c62496b560a11baaba683bec0
unknown <abhishekservi586@gmail.com>
2024-07-23 01:08:19 +0530

    Added Tailwind and Props

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+    let [counter, setCounter] = useState(15)
+
+  // let counter = 15
+
+  const addValue = () => {
+    if (counter>=20){
+      counter=20
+      console.log("Counter Value is maximum");
+    }
+    else{
+      setCounter(counter+1)
+    }
+  }
+
+  const removeValue = () => {
+    if (counter<=0) {
+      counter = 0
+      console.log("Counter value cannot be negative");
+    }
+    else{
+      setCounter(counter-1)
+    }
+    
+  }
+
+  return (
+    <>
+     <h1>PRESS THAT BUTTON  </h1>
+     <p>Counter Value:{counter}</p>
+     <button 
+     onClick={addValue}>Add Value {counter}</button>
+     <br />
+     <button
+     onClick={removeValue}>Remove Value {counter}</button>
+     <p>Footer: {counter}</p>
+    </>
+
+  )
+}
+
+export default App
