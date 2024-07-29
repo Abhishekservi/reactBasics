6c7bc73ce0f9ffde20c0ecd1242a2e8a84dff5c0
abhishekservi586@gmail.com <abhishekservi586@gmail.com>
2024-07-29 16:38:56 +0530

    Creating MegaBlog App

---
+++ 13MegaBlog/src/components/index.js
@@ -0,0 +1,12 @@
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import Container from './container/Container';
+import Logo from './Logo';
+import LogoutBtn from './Header/LogoutBtn';
+import Button from './Button';
+import PostCard from './PostCard';
+import Login from './Login';
+import Input from './Input';
+import Select from './Select';
+
+export { Header, Footer, Container, Logo, LogoutBtn, Input, Button, PostCard, Login, Select };
